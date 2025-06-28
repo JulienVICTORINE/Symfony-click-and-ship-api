78cebc82207071b0bb410586d4dcbeb65388bb14
julien <julien.victorine440@gmail.com>
2025-06-28 15:40:09 +0400

    first commit, création entité Product & migration database + fixtures for Product

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        for ($i = 1; $i <= 5; $i++) {
+            $product = new Product();
+            $product->setName("Produit $i");
+            $product->setDescription("Description du produit $i");
+            $product->setPrice(mt_rand(10, 100));
+            $product->setImage("https://via.placeholder.com/150");
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
